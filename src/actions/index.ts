088ca7fee679d3bcd03f0dfc7a90a512eb5cadd4
maxwel kirimi <maxwelkirimii@gmail.com>
2025-02-01 17:06:59 +0300

    update

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,31 +1,29 @@
 import { redirect } from "react-router-dom";
 import customFetch from "../axios/custom";
 
-interface SearchActionRequest {
+interface FormRequest {
   request: {
     formData: () => Promise<FormData>;
   };
 }
 
-interface CheckoutFormAction {
-  request: {
-    formData: () => Promise<FormData>;
-  };
-}
-
-export const searchAction = async ({ request }: SearchActionRequest) => {
-  // getting form data
+export const searchAction = async ({ request }: FormRequest) => {
   const formData = await request.formData();
-  // converting form data to object for easy access
   const data = Object.fromEntries(formData);
-
+  
   return redirect(`/search?query=${data?.searchInput || ""}`);
 };
 
-
-export const checkoutAction = async ({request} : CheckoutFormAction) => {
+export const checkoutAction = async ({ request }: FormRequest) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  const response = await customFetch.post("/orders", data);
+
+  try {
+    const response = await customFetch.post("/orders", data);
+    console.log(`Order response status: ${response.status}`);
+  } catch (error) {
+    console.error("Error processing order:", error);
+  }
+
   return redirect('/');
-}
+};
